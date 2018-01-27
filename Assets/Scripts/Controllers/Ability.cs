d30a77748a52c9483b2640f385db1c6a7049e791
Jay Suong <psuong95@gmail.com>
2018-01-27 16:52:57 -0500

    made ability and ability manager script

---
+++ Assets/Scripts/Controllers/Ability.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace boc {
+	public abstract class Ability : ScriptableObject {
+		public virtual void OnAbilityStart () { }
+		public virtual void OnAbilityUpdate () { }
+		public virtual void OnAbilityEnd () { }
+
+	}
+}
