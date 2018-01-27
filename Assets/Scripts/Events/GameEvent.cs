3be95053aaee52f412e9675b40d0351c8034e051
dqdt <sw2384@nyu.edu>
2018-01-27 11:26:17 -0500

    GameEvent.cs

---
+++ Assets/Scripts/Events/GameEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace boc {
+	[CreateAssetMenu (menuName = "ScriptableObjects/GameEvent")]
+	public class GameEvent : ScriptableObject {
+
+		[SerializeField, TextArea] private string comment;
+		private event Action OnGameEventInvoke;
+
+		public void Invoke () {
+			if (OnGameEventInvoke != null)
+				OnGameEventInvoke ();
+		}
+
+		public void Register (Action handler) {
+			// Make sure a handler is only run once.
+			if (!OnGameEventInvoke.GetInvocationList ().Contains (handler))
+				OnGameEventInvoke += handler;
+		}
+
+		public void Unregister (Action handler) {
+			OnGameEventInvoke -= handler;
+		}
+	}
+}
