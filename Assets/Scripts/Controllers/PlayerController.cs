415bdf5772911f4a17627416200caac9d8c6a73a
Jay Suong <psuong95@gmail.com>
2018-01-27 14:10:24 -0500

    Added a basic controller script

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace boc {
+	public class PlayerController : MonoBehaviour {
+
+		[SerializeField]
+		private bool isActive;
+
+		[SerializeField, Header ("Controls")]
+		private string horizontalName = "Horizontal";
+		[SerializeField]
+		private string verticalName = "Vertical";
+
+		private CharacterController controller;
+
+		private void Awake () {
+			controller = GetComponent<CharacterController> ();
+		}
+
+		private void Update () {
+			if (!isActive) { return; }
+		}
+
+	}
+}
