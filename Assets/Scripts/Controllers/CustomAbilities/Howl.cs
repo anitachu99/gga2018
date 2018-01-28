f84558db3e59f6daa1b0ca4b1f1016e039f400ef
Jay Suong <jaysuong@gmail.com>
2018-01-27 23:44:26 -0500

    Added a cooldown ability and converted abilities to use the new script

--- Assets/Scripts/Controllers/CustomAbilities/Howl.cs
+++ Assets/Scripts/Controllers/CustomAbilities/Howl.cs
@@ -2,32 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace boc {[CreateAssetMenu (menuName = "ScriptableObjects/abilities/howling")]
-	public class Howl : Ability {
+namespace boc {
+	[CreateAssetMenu (menuName = "ScriptableObjects/abilities/howling")]
+	public class Howl : CooldownAbility {
 		[SerializeField]
 		private float amountToHeal = 30;
 		[SerializeField]
 		private AudioClip howlingSound;
 		[SerializeField]
-		private float cooldown;
+		private KeyCode abilitykey;
+
 		private IHealth health;
 		private AudioSource source;
-		[SerializeField]
-		private KeyCode abilitykey;
-		private float cooldowntimer;
+
 		public override void OnAbilityStart (GameObject player) {
 			health = player.GetComponent<IHealth> ();
 			source = player.GetComponent<AudioSource> ();
+			currentStage = AbilityStage.Idle;
 		}
 		public override void OnAbilityUpdate (GameObject player) {
-			cooldowntimer -= Time.deltaTime;
-			if (cooldowntimer <= 0) {
-				if (Input.GetKeyDown (abilitykey)) {
-					health.Heal (amountToHeal);
-					source.clip = howlingSound;
-					source.Play ();
-					cooldowntimer = cooldown;
-				}
+			switch (currentStage) {
+				case AbilityStage.Idle:
+					if (Input.GetKeyDown (abilitykey)) {
+						source.clip = howlingSound;
+						source.Play ();
+
+						abilityTimer = duration;
+						currentStage = AbilityStage.Active;
+					}
+					break;
+
+				case AbilityStage.Active:
+					abilityTimer -= Time.deltaTime;
+
+					if (abilityTimer <= 0f) {
+						health.Heal (amountToHeal);
+						cooldownTimer -= cooldown;
+					}
+					break;
+
+				case AbilityStage.Cooldown:
+					cooldownTimer -= Time.deltaTime;
+
+					if (cooldownTimer <= 0f) {
+						currentStage = AbilityStage.Idle;
+					}
+
+					break;
 			}
 		}
 	}
--- Assets/Scripts/Controllers/CustomAbilities/SpeedBoost.cs
+++ Assets/Scripts/Controllers/CustomAbilities/SpeedBoost.cs
@@ -3,55 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace boc {
 	[CreateAssetMenu (menuName = "ScriptableObjects/abilities/speedboost")]
-	public class SpeedBoost : Ability {
+	public class SpeedBoost : CooldownAbility {
 		[SerializeField]
 		private float speedmultiplier = 5;
 		[SerializeField]
-		private float abilityduration = 5;
-		[SerializeField]
-		private float cooldown = 5;
-		[SerializeField]
 		private KeyCode abilityKey;
-		private float abilitytimer;
-		private float cooldowntimer;
+
 		private float oldspeed;
-		private AbilityStage currentstage;
 		private PlayerController controller;
-		public enum AbilityStage {
-			Idle,
-			Active,
-			Cooldown
-		}
 
 		public override void OnAbilityStart (GameObject player) {
 			controller = player.GetComponent<PlayerController> ();
 		}
+
 		public override void OnAbilityUpdate (GameObject player) {
-			switch (currentstage) {
+			switch (currentStage) {
 				case AbilityStage.Idle:
 					if (Input.GetKeyDown (abilityKey)) {
 						oldspeed = controller.Speed;
 						controller.Speed = oldspeed * speedmultiplier;
-						abilitytimer = abilityduration;
-						currentstage = AbilityStage.Active;
+						abilityTimer = duration;
+						currentStage = AbilityStage.Active;
 					}
 					break;
+
 				case AbilityStage.Active:
-					abilitytimer -= Time.deltaTime;
-					if (abilitytimer <= 0) {
+					abilityTimer -= Time.deltaTime;
+
+					if (abilityTimer <= 0) {
 						controller.Speed = oldspeed;
-						cooldowntimer = cooldown;
-						currentstage = AbilityStage.Cooldown;
+						cooldownTimer = cooldown;
+						currentStage = AbilityStage.Cooldown;
 
 					}
 					break;
+
 				case AbilityStage.Cooldown:
-					cooldowntimer -= Time.deltaTime;
-					if (cooldowntimer <= 0) {
-						currentstage = AbilityStage.Idle;
+					cooldownTimer -= Time.deltaTime;
+
+					if (cooldownTimer <= 0) {
+						currentStage = AbilityStage.Idle;
 					}
 					break;
-
 			}
 		}
 
