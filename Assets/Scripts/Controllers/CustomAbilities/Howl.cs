dda46f6dd98a8e0babce63c637295a971b19b29b
Jay Suong <psuong95@gmail.com>
2018-01-27 20:13:12 -0500

    Added howling

--- Assets/Scripts/Controllers/CustomAbilities/Howl.cs
+++ Assets/Scripts/Controllers/CustomAbilities/Howl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace boc {
+namespace boc {[CreateAssetMenu (menuName = "ScriptableObjects/abilities/howling")]
 	public class Howl : Ability {
 		[SerializeField]
 		private float amountToHeal = 30;
@@ -10,6 +10,25 @@ namespace boc {
 		private AudioClip howlingSound;
 		[SerializeField]
 		private float cooldown;
-		
+		private IHealth health;
+		private AudioSource source;
+		[SerializeField]
+		private KeyCode abilitykey;
+		private float cooldowntimer;
+		public override void OnAbilityStart (GameObject player) {
+			health = player.GetComponent<IHealth> ();
+			source = player.GetComponent<AudioSource> ();
+		}
+		public override void OnAbilityUpdate (GameObject player) {
+			cooldowntimer -= Time.deltaTime;
+			if (cooldowntimer <= 0) {
+				if (Input.GetKeyDown (abilitykey)) {
+					health.Heal (amountToHeal);
+					source.clip = howlingSound;
+					source.Play ();
+					cooldowntimer = cooldown;
+				}
+			}
+		}
 	}
 }
