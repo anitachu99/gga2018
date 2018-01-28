53e1a0f75642ae61f5e24e782ede24ca33857c33
Jay Suong <psuong95@gmail.com>
2018-01-27 19:01:02 -0500

    Added howl stub

---
+++ Assets/Scripts/Controllers/CustomAbilities/Howl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace boc {
+	public class Howl : Ability {
+		[SerializeField]
+		private float amountToHeal = 30;
+		[SerializeField]
+		private AudioClip howlingSound;
+		[SerializeField]
+		private float cooldown;
+		
+	}
+}
